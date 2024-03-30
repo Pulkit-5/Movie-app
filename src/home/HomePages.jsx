3df843176f226de73fca9d6891e1b3cec1bd40bc
Pulkit-5 <pulkit.v515@gmail.com>
2024-03-30 14:56:40 +0530

    first commit

---
+++ src/home/HomePages.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import { Homes } from "../components/homes/Homes"
+import { homeData } from "../dummyData"
+const HomePages = () => {
+  const[items,setItems] = useState(homeData)
+  return (<>
+  
+  </>)
+}
+
+export default HomePages
